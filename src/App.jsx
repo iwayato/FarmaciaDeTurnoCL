6bca4ac11dfd0e06b4e06d55e1c7fce4e9bfb7d4
Tomoaki Iwaya <iwayatomoaki@gmail.com>
2024-06-01 02:08:20 -0400

    Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,13 @@
+import {
+    Grid,
+    GridItem,
+    HStack,
+    Text,
+    Link,
+    Heading,
+    Center,
+    Spinner
+} from "@chakra-ui/react";
 import { useState, useEffect } from "react";
 import axios from "axios";
 import Map from "./components/Map";
@@ -9,8 +19,24 @@ const App = () => {
     const [data, setData] = useState(null);
     const [isLoading, setIsLoading] = useState(true);
     const [error, setError] = useState(null);
+    const [userLoc, setUserLoc] = useState({
+        lat: -34.135020,
+        lng: -71.565964
+    });
+
+    const successCallback = (position) => {
+        setUserLoc({
+            lat: position.coords.latitude, 
+            lng: position.coords.longitude
+        });
+    };
+
+    const errorCallback = (error) => {
+        console.log(error);
+    };
 
     useEffect(() => {
+        navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
         const fetchData = async () => {
             try {
                 setIsLoading(true);
@@ -28,18 +54,63 @@ const App = () => {
 
     if (isLoading) {
         return (
-            <div>
-                Loading ...
-            </div>
+            <Center width='100%' height='100vh'>
+                <Spinner
+                    height='150px'
+                    width='150px'
+                    thickness='4px'
+                    speed='0.65s'
+                    emptyColor='gray.200'
+                    color='blue.500'
+                    size='xl' />
+            </Center>
         )
     }
 
     if (error) {
-        return <div>Error: {error.message}</div>;
+        return (
+            <Center width='100%' height='100vh'>
+                <Text fontSize={"larger"} fontWeight={"bold"}>
+                    Error: {error.message}
+                </Text>
+            </Center>
+        )
     }
 
     return (
-        <Map spots = {data}/>
+        <Grid
+            templateAreas={`"header header" "nav main" "nav footer"`}
+            gridTemplateRows={'50px 1fr 30px'}
+            gridTemplateColumns={'0px 1fr'}
+            h={"100vh"}
+            color='blackAlpha.700'
+            fontWeight='bold'>
+
+            <GridItem pl='10px' bg='gray.100' area={'header'}>
+                <Heading>Farmacias en turno CL</Heading>
+            </GridItem>
+
+            <GridItem bg='green.300' area={'main'}>
+                <Map spots={data} center={userLoc}/>
+            </GridItem>
+
+            <GridItem pl='2' bg='gray.600' area={'footer'} color='white'>
+                <HStack>
+                    <Text marginTop='3px'>
+                        Realizado por {' '} {' '}
+                        <Link color='teal.500' href='https://github.com/iwayato'>
+                            Tomoaki Iwaya Villalobos
+                        </Link>
+                    </Text>
+                    <Text>
+                        -
+                    </Text>
+                    <Text>
+                        2024
+                    </Text>
+                </HStack>
+            </GridItem>
+        </Grid>
     );
 }
 
--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,5 +1,5 @@
 import { MapContainer, TileLayer, Marker, Tooltip } from 'react-leaflet';
-import { VStack, Text, Heading, Divider } from '@chakra-ui/react';
+import { VStack, Text, Heading } from '@chakra-ui/react';
 import L from 'leaflet';
 import "leaflet/dist/leaflet.css";
 
@@ -7,17 +7,17 @@ const corner1 = L.latLng(-11.011774, -102.208789);
 const corner2 = L.latLng(-57.234297, -25.698563);
 const bounds = L.latLngBounds(corner1, corner2);
 
-const Map = ({ spots }) => {
+const Map = ({ spots, center }) => {
 
     return (
         <MapContainer
-            style={{ height: '100vh', width: '100vw' }}
+            style={{ height: '100%', width: '100%' }}
             maxBounds={bounds}
             zoomControl={true}
             minZoom={4}
             dragging={true}
-            center={[-34.135020, -71.565964]}
-            zoom={4}
+            center={[center.lat, center.lng]}
+            zoom={15}
             scrollWheelZoom={true}>
 
             <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
@@ -34,11 +34,16 @@ const Map = ({ spots }) => {
                                 spacing={3}
                                 align='stretch'>
                                 <Heading size='md' color='cadetblue'>{spot.local_nombre}</Heading>
-                                <Divider></Divider>
                                 <Text fontSize='sm' color='gray.1000'>{spot.local_direccion}</Text>
-                                <Text fontSize='sm' color='gray.1000'>Hora apertura: {spot.funcionamiento_hora_apertura}</Text>
-                                <Text fontSize='sm' color='gray.1000'>Hora cierre: {spot.funcionamiento_hora_cierre}</Text>
-                                <Text fontSize='sm' color='gray.1000'>Teléfono: {spot.local_telefono}</Text>
+                                <Text fontSize='sm' color='gray.1000'>Hora apertura: {spot.funcionamiento_hora_apertura.slice(0, -3)}</Text>
+                                <Text fontSize='sm' color='gray.1000'>Hora cierre: {spot.funcionamiento_hora_cierre.slice(0, -3)}</Text>
+                                <Text fontSize='sm' color='gray.1000'>Teléfono: {
+                                    spot.local_telefono ?
+                                        (spot.local_telefono === "+56" || spot.local_telefono === "+560" ?
+                                            "Sin contacto" : spot.local_telefono)
+                                        : "Sin contacto"
+                                }
+                                </Text>
                             </VStack>
                         </Tooltip>
                     </Marker>
