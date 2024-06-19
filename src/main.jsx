09f38b56ff41661b7227b7d70fb665ab03ef5705
Tomoaki Iwaya <iwayatomoaki@gmail.com>
2024-06-18 20:28:45 -0400

    Added Vercel Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { ChakraProvider } from '@chakra-ui/react'
+import { Analytics } from "@vercel/analytics/react"
 import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <ChakraProvider>
         <App />
+        <Analytics/>
     </ChakraProvider>
 )
